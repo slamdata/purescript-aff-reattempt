249a49bbbdffdf31ec70ec3e2e0458e1a7a84084
Daniel Chambers <daniel@digitallycreated.net>
2016-07-15 12:23:40 +1000

    Upgraded project to PureScript v0.9.1, and updated all dependencies
    Biggest change was in the unit tests which were restructured to use the latest Test.Unit

--- test/Main.purs
+++ test/Main.purs
@@ -4,18 +4,26 @@ import Prelude
 import Control.Apply ((*>))
 import Control.Alt ((<|>))
 import Control.Monad.Aff.AVar (makeVar', takeVar, putVar, AVar(), AVAR())
-import Control.Monad.Aff.Par (Par(..), runPar)
-import Control.Monad.Aff (attempt, later, later', launchAff, Aff())
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Error.Class (catchError)
+import Control.Monad.Aff (attempt, later, later', Aff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Parallel.Class (parallel, runParallel)
 import Control.Plus (empty)
-import Data.Either (Either(..), either)
+import Data.Either as Either
 import Data.Functor (($>))
 import Data.Maybe (maybe, fromMaybe)
-import Data.Array (replicate, head, tail)
-import Test.Unit (runTest, test, assertFn)
+import Data.Array (head, tail)
+import Data.Unfoldable (replicate)
+import Test.Unit (test, suite)
+import Test.Unit.Assert (assert)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Main (runTest)
 
 import Control.Monad.Aff.Reattempt (reattempt)
+
+ignore :: forall m a. Monad m => m a -> m Unit
+ignore m = m >>= (\_ -> pure unit)
 
 failAffsForDurationAndNumberOfAttempts :: forall e. Int -> Int -> Array (Aff e Unit)
 failAffsForDurationAndNumberOfAttempts timeout attemptCount = seq
@@ -29,44 +37,34 @@ affDouble affsVar = do
   putVar affsVar (fromMaybe [] (tail affs))
   maybe (pure unit) id $ head affs
 
+main :: forall eff. Eff (console :: CONSOLE, testOutput :: TESTOUTPUT, ref :: REF, avar :: AVAR | eff) Unit
 main = runTest do
-  test "When the Aff never succeeds" do
+  test "When the Aff never succeeds the returned Aff should fail" do
+    result <- attempt $ reattempt 100 (later empty)
+    assert "The returned Aff did not fail" $ Either.isLeft result
 
-    assertFn "The returned Aff should fail" \done ->
-      launchAff $ do
-        result <- attempt $ reattempt 100 (later empty)
-        liftEff $ either (const $ done true) (const $ done false) result
+  test "When the timeout will elapse before any attempts to run the Aff are successful the returned Aff should fail" do
+    seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 1000 10
+    result <- attempt $ reattempt 100 (affDouble seq)
+    assert "The returned Aff did not fail" $ Either.isLeft result
 
-  test "When the timeout will elapse before any attempts to run the Aff are successful" do
+  test "When the Aff always succeeds the returned Aff must be successful" do
+    result <- attempt $ reattempt 10000 (later' 1000 $ pure unit)
+    assert "The returned Aff failed" $ Either.isRight result
 
-    assertFn "The returned Aff should fail" \done ->
-      launchAff $ do
-        seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 1000 10
-        result <- attempt $ reattempt 100 (affDouble seq)
-        liftEff $ either (const $ done true) (const $ done false) result
+  suite "When the Aff will succeed during an attempt started before the timeout will elapse" do
 
-  test "When the Aff always succeeds" do
+    test "The returned Aff should be successful" do
+      seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 100 10
+      result <- attempt $ reattempt 100000000 (affDouble seq)
+      assert "The returned Aff failed" $ Either.isRight result
 
-    assertFn "The returned Aff be successful" \done ->
-      launchAff $ do
-        result <- attempt $ reattempt 10000 (later' 1000 $ pure unit)
-        liftEff $ either (const $ done false) (const $ done true) result
-
-  test "When the Aff will succeed during an attempt started before the timeout will elapse" do
-
-    assertFn "The returned Aff should be successful" \done ->
-      launchAff $ do
-        seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 100 10
-        result <- attempt $ reattempt 100000000 (affDouble seq)
-        liftEff $ either (const $ done false) (const $ done true) result
-
-    assertFn "The returned Aff should not wait for the timeout to elapse in order to succeed" \done ->
-      launchAff $ do
-        seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 100 10
-        let parReattempt = Par (reattempt 100000000 (affDouble seq) $> true)
-        let parLater = Par (later' 1000 $ pure false)
-        result <- runPar $ parReattempt <|> parLater
-        liftEff $ done result
+    test "The returned Aff should not wait for the timeout to elapse in order to succeed" do
+      seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 100 10
+      let parReattempt = parallel (reattempt 100000000 (affDouble seq) $> true)
+      let parLater = parallel (later' 1000 $ pure false)
+      result <- runParallel $ parReattempt <|> parLater
+      assert "The returned Aff failed" result
 
   -- TODO: Test that process finishes when attempt is successful.
 
