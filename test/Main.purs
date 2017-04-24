b96c3e9485083060790eeba4569394a0aeafb4f8
Gary Burgess <gary.burgess@gmail.com>
2017-04-24 12:26:10 +0100

    Update for PureScript 0.11

--- test/Main.purs
+++ test/Main.purs
@@ -1,19 +1,20 @@
 module Test.Main where
 
 import Prelude
-import Control.Apply ((*>))
 import Control.Alt ((<|>))
+import Control.Monad.Aff (attempt, delay, Aff)
 import Control.Monad.Aff.AVar (makeVar', takeVar, putVar, AVar(), AVAR())
-import Control.Monad.Aff (attempt, later, later', Aff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Console (CONSOLE)
-import Control.Parallel.Class (parallel, runParallel)
+import Control.Monad.Eff.Ref (REF)
+import Control.Parallel (parallel, sequential)
 import Control.Plus (empty)
-import Data.Either as Either
-import Data.Functor (($>))
-import Data.Maybe (maybe, fromMaybe)
 import Data.Array (head, tail)
+import Data.Either as Either
+import Data.Int as Int
+import Data.Maybe (maybe, fromMaybe)
+import Data.Newtype (unwrap)
+import Data.Time.Duration (Milliseconds(..))
 import Data.Unfoldable (replicate)
 import Test.Unit (test, suite)
 import Test.Unit.Assert (assert)
@@ -22,11 +23,13 @@ import Test.Unit.Main (runTest)
 
 import Control.Monad.Aff.Reattempt (reattempt)
 
-failAffsForDurationAndNumberOfAttempts :: forall e. Int -> Int -> Array (Aff e Unit)
+failAffsForDurationAndNumberOfAttempts :: forall e. Milliseconds -> Int -> Array (Aff e Unit)
 failAffsForDurationAndNumberOfAttempts timeout attemptCount = seq
   where
-  seq = replicate attemptCount (later' interval (empty))
-  interval = timeout / attemptCount
+  seq = replicate attemptCount do
+    delay interval
+    empty
+  interval = Milliseconds (unwrap timeout / Int.toNumber attemptCount)
 
 affDouble :: forall e. AVar (Array (Aff (avar :: AVAR | e) Unit)) -> Aff (avar :: AVAR | e) Unit
 affDouble affsVar = do
@@ -37,30 +40,37 @@ affDouble affsVar = do
 main :: forall eff. Eff (console :: CONSOLE, testOutput :: TESTOUTPUT, ref :: REF, avar :: AVAR | eff) Unit
 main = runTest do
   test "When the Aff never succeeds the returned Aff should fail" do
-    result <- attempt $ reattempt 100 (later empty)
+    result <- attempt $ reattempt (Milliseconds 100.0) do
+      delay (Milliseconds 15.0)
+      empty
     assert "The returned Aff did not fail" $ Either.isLeft result
 
   test "When the timeout will elapse before any attempts to run the Aff are successful the returned Aff should fail" do
-    seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 1000 10
-    result <- attempt $ reattempt 100 (affDouble seq)
+    seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts (Milliseconds 1000.0) 10
+    result <- attempt $ reattempt (Milliseconds 100.0) (affDouble seq)
     assert "The returned Aff did not fail" $ Either.isLeft result
 
   test "When the Aff always succeeds the returned Aff must be successful" do
-    result <- attempt $ reattempt 10000 (later' 1000 $ pure unit)
+    result <- attempt $ reattempt (Milliseconds 10000.0) do
+      delay (Milliseconds 1000.0)
+      pure unit
     assert "The returned Aff failed" $ Either.isRight result
 
   suite "When the Aff will succeed during an attempt started before the timeout will elapse" do
 
     test "The returned Aff should be successful" do
-      seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 100 10
-      result <- attempt $ reattempt 100000000 (affDouble seq)
+      seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts (Milliseconds 100.0) 10
+      result <- attempt $ reattempt (Milliseconds 100000000.0) (affDouble seq)
       assert "The returned Aff failed" $ Either.isRight result
 
     test "The returned Aff should not wait for the timeout to elapse in order to succeed" do
-      seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts 100 10
-      let parReattempt = parallel (reattempt 100000000 (affDouble seq) $> true)
-      let parLater = parallel (later' 1000 $ pure false)
-      result <- runParallel $ parReattempt <|> parLater
+      seq <- makeVar' $ failAffsForDurationAndNumberOfAttempts (Milliseconds 100.0) 10
+      let
+        parReattempt = parallel (reattempt (Milliseconds 100000000.0) (affDouble seq) $> true)
+        parLater = parallel do
+          delay (Milliseconds 1000.0)
+          pure false
+      result <- sequential $ parReattempt <|> parLater
       assert "The returned Aff failed" result
 
   -- TODO: Test that process finishes when attempt is successful.
